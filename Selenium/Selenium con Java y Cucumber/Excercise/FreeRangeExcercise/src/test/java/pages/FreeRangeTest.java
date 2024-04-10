ebd1d6b3786b8dad05b75ad2002ffee447147bbe
GustavoBitterly <gustavo.bitterly@icloud.com>
2024-04-10 13:06:04 -0400

    Se actualiza y eliminan archivos para curso de Selenium

---
+++ Selenium/Selenium con Java y Cucumber/Excercise/FreeRangeExcercise/src/test/java/pages/FreeRangeTest.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FreeRangeTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        //Inicializa el WebDriver para Edge
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+    }
+
+    @Test
+    public void navegamosAGoogle(){
+        //Navega a la pagina Web
+        driver.get("https://www.google.cl");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        //Cierra el navegador despues de la prueba
+        if (driver != null){
+            driver.quit();
+        }
+    }
+}
