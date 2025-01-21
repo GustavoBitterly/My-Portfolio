5c2ff7908701eb35072d8e4822bf1781032c21f9
GustavoBitterly <gustavo.bitterly@icloud.com>
2025-01-20 23:15:58 -0300

    Se actualiza ficheros de ejercicios de Java y Python

---
+++ Codedex_Courses/Java/Exercises/03_cooking_mama.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    // Write your code here 💖
+    System.out.println("I walked through the door with you");
+    System.out.println("the air was cold");
+    System.out.println("But something about it felt like home somehow");
+  }
+}
---
+++ Codedex_Courses/Java/Exercises/04_error_logs.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    // Write Your code here 💖
+
+    // Welcome message for users
+    System.out.println("Hey there, friend!");
+
+    System.out.println("What is the current version?"); // Version 18.9.2
+
+    /*
+        System.out.println("The location can’t be updated.");
+        System.out.println("The location is not up to date.");
+        */
+    System.out.println("The location is Chicago, IL");
+
+    System.err.println("Connecting to server…");
+    System.err.println("Error: Connection timed out.");
+    System.err.println("Retrying…");
+    // System.err.println() is used to print error messages! 😵😵❌
+    // It is similar to System.out.println() but is used to print error messages.
+
+  }
+}
