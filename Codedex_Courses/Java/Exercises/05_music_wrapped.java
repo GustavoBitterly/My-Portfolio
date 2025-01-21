8eb995b0e40bce76a9b076a6b942fa4e43451df2
Gustavo Bitterly Pavez <gustavo.bitterly@icloud.com>
2025-01-21 11:54:17 -0300

    Create 05_music_wrapped.java

---
+++ Codedex_Courses/Java/Exercises/05_music_wrapped.java
@@ -0,0 +1,19 @@
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * Write a little bit about yourself here. What's your name? Where are you from? What do you do?
+ *  
+ * I'm a comment, and I want to say hi! 👋
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        // Write your code here! 💖
+        System.out.println("Seeing Red"); // Architecs
+        System.out.println("Adieu"); // Ramsstein
+        System.out.println("Whiplash"); // Architecs
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
