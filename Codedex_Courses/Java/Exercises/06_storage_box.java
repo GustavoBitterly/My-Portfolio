67f66ac7cb59fe9b4520c52784b807087beea1f2
Gustavo Bitterly Pavez <gustavo.bitterly@icloud.com>
2025-01-21 11:54:46 -0300

    Create 06_storage_box.java

---
+++ Codedex_Courses/Java/Exercises/06_storage_box.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        int livingRoomBox = 15;
+        int masterBedroomBox = 45;
+        int bathroom = 10;
+
+        System.out.println(livingRoomBox);
+    }
+}
