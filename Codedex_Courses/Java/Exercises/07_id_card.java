a16020e6af63af1df50f1e438aa08a7c4864ad32
Gustavo Bitterly Pavez <gustavo.bitterly@icloud.com>
2025-01-21 11:58:21 -0300

    Create 07_id_card.java

---
+++ Codedex_Courses/Java/Exercises/07_id_card.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        String name = "Gustavo Bitterly";
+        String state = "CHL";
+        char gender = "M";
+        int age = 27;
+        boolean = true;
+
+        System.out.println(name);
+    }
+}
