134a90dc0126ae77635fddb92ac5fed799102355
Gustavo Bitterly Pavez <gustavo.bitterly@icloud.com>
2025-01-21 12:33:00 -0300

    Create 10_club_penguin.java

---
+++ Codedex_Courses/Java/Exercises/10_club_penguin.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        Scanner scanner = new Scanner(System.in);
+
+        double usdValue = 0.0045;
+        int clubPenguinCoin = 0;
+        double conversionResult = 0;
+
+        System.out.println("Welcome to Club Penguin Conversion Calculator");
+        System.out.println("Currency: 1 Coin = $0.0045 USD");
+
+        System.out.print("How many Club Penguin Coins do yo have?");
+
+        clubPenguinCoin = scanner.nextLine(); // int values
+
+        System.out.print("Calculating.....");
+        conversionResult = (clubPenguinCoin*usdValue)
+
+        System.out.print("You have: $" + conversionResult + " USD in value");
+
+    }
+}
